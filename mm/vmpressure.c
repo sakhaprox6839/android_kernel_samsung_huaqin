88d33ff382e67da9eedd8505c32af5b8cc5510cb
sakhaprox <130843051+sakhaprox6839@users.noreply.github.com>
2025-12-17 00:41:04 +0700

    Refactor vmpressure handling for scanned condition

--- mm/vmpressure.c
+++ mm/vmpressure.c
@@ -368,36 +368,38 @@ static void vmpressure_global(gfp_t gfp, unsigned long scanned,
 	unsigned long pressure;
 	unsigned long stall;
 
-	if (!scanned)
-		return;
-
-	spin_lock(&vmpr->sr_lock);
-	if (!vmpr->scanned)
-		calculate_vmpressure_win();
+	if (scanned) {
+		spin_lock(&vmpr->sr_lock);
+		if (!vmpr->scanned)
+			calculate_vmpressure_win();
 	
-	vmpr->scanned += scanned;
-	vmpr->reclaimed += reclaimed;
+		vmpr->scanned += scanned;
+		vmpr->reclaimed += reclaimed;
 
-	if (!current_is_kswapd())
-		vmpr->stall += scanned;
+		if (!current_is_kswapd())
+			vmpr->stall += scanned;
 
-	stall = vmpr->stall;
+		stall = vmpr->stall;
+		scanned = vmpr->scanned;
+		reclaimed = vmpr->reclaimed;
+		spin_unlock(&vmpr->sr_lock);
+
+		if (scanned < vmpressure_win)
+			return;
+	}
 	
-	scanned = vmpr->scanned;
-	reclaimed = vmpr->reclaimed;
-	spin_unlock(&vmpr->sr_lock);
-
-	if (scanned < vmpressure_win)
-		return;
-
 	spin_lock(&vmpr->sr_lock);
 	vmpr->scanned = 0;
 	vmpr->reclaimed = 0;
 	vmpr->stall = 0;
 	spin_unlock(&vmpr->sr_lock);
 
-	pressure = vmpressure_calc_pressure(scanned, reclaimed);
-	pressure = vmpressure_account_stall(pressure, stall, scanned);
+	if (scanned) {
+		pressure = vmpressure_calc_pressure(scanned, reclaimed);
+		pressure = vmpressure_account_stall(pressure, stall, scanned);
+	} else {
+		pressure = 100;
+	}
 	vmpressure_notify(pressure);
 }
 
